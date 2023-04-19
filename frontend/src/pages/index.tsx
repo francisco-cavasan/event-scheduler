503057fa2b8ad10bcd161a596204d15cb892e897
Francisco Cavasan <francisco.cavasan12@gmail.com>
2023-04-19 01:59:58 -0300

    fix: :bug: update frontend files

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center">
-      <span>teste</span>
-    </div>
-  )
+	return (
+		<div>
+			<span>hello</span>
+		</div>
+	);
 }
